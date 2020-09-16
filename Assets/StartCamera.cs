aa8e20d3b4f62c1309e467f3d216f5251f06fc3d
Sawyer Smith <sps471@nyu.edu>
2020-09-16 15:31:49 -0400

    9-16-2020

---
+++ Assets/StartCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class StartCamera : MonoBehaviour
+{
+    Camera camera1;
+    Camera camera2;
+    Camera camera3;
+    Camera camera4;
+    Camera camera5;
+    Camera camera6;
+    Camera camera7;
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        camera1.enabled = true;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
+        camera5.enabled = false;
+        camera6.enabled = false;
+        camera7.enabled = false;
+    }
+}
+
+
